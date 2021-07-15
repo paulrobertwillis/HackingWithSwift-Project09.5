056a6dd3773e71f56eb87016cea3bc86e0e407d4
Paul Willis <ios_dev@paulrobertwillis.com>
2021-07-15 12:54:58 +0100

    Answer text field added, Auto Layout constraints applied

--- Project09.5/ViewController.swift
+++ Project09.5/ViewController.swift
@@ -8,6 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var livesLeft = 0
+    var livesLabel: UILabel!
+    var currentAnswer: UITextField!
+//    var answer: String
+    var letterButtons = [UIButton]()
+    
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+
+        livesLabel = UILabel()
+        livesLabel.translatesAutoresizingMaskIntoConstraints = false
+        livesLabel.textAlignment = .right
+        livesLabel.text = "Lives Left: \(livesLeft)"
+        view.addSubview(livesLabel)
+        
+        currentAnswer = UITextField()
+        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+        currentAnswer.placeholder = "Tap letters to guess"
+        currentAnswer.textAlignment = .center
+        currentAnswer.font = UIFont.systemFont(ofSize: 44)
+        currentAnswer.isUserInteractionEnabled = false
+        view.addSubview(currentAnswer)
+        
+
+
+
+        NSLayoutConstraint.activate([
+            livesLabel.topAnchor.constraint(equalTo:
+            view.layoutMarginsGuide.topAnchor),
+            livesLabel.trailingAnchor.constraint(equalTo:
+            view.layoutMarginsGuide.trailingAnchor),
+
+            currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentAnswer.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+        ])
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
