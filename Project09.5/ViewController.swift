86255b80588fb0054a971627931569cadd3c68f9
Paul Willis <ios_dev@paulrobertwillis.com>
2021-07-15 13:08:06 +0100

    added interfaceView to contain text field and two buttons

--- Project09.5/ViewController.swift
+++ Project09.5/ViewController.swift
@@ -18,7 +18,15 @@ class ViewController: UIViewController {
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
+        
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsView)
 
+        let interfaceView = UIView()
+        interfaceView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(interfaceView)
+        
         livesLabel = UILabel()
         livesLabel.translatesAutoresizingMaskIntoConstraints = false
         livesLabel.textAlignment = .right
@@ -32,7 +40,17 @@ class ViewController: UIViewController {
         currentAnswer.font = UIFont.systemFont(ofSize: 44)
         currentAnswer.isUserInteractionEnabled = false
         view.addSubview(currentAnswer)
+        currentAnswer.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         
+        let submit = UIButton(type: .system)
+        submit.translatesAutoresizingMaskIntoConstraints = false
+        submit.setTitle("SUBMIT", for: .normal)
+        view.addSubview(submit)
+
+        let clear = UIButton(type: .system)
+        clear.translatesAutoresizingMaskIntoConstraints = false
+        clear.setTitle("CLEAR", for: .normal)
+        view.addSubview(clear)
 
 
 
@@ -43,9 +61,29 @@ class ViewController: UIViewController {
             view.layoutMarginsGuide.trailingAnchor),
 
             currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            currentAnswer.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            currentAnswer.topAnchor.constraint(equalTo: livesLabel.bottomAnchor),
+            
+            submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+            submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+            submit.heightAnchor.constraint(equalToConstant: 44),
+
+            clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
+            clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
+            clear.heightAnchor.constraint(equalToConstant: 44),
+            
+            buttonsView.widthAnchor.constraint(equalToConstant: 750),
+            buttonsView.heightAnchor.constraint(equalToConstant: 320),
+            buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsView.topAnchor.constraint(equalTo: submit.bottomAnchor,
+            constant: 20),
+            buttonsView.bottomAnchor.constraint(equalTo:
+            view.layoutMarginsGuide.bottomAnchor, constant: -20)
         ])
+        
+        buttonsView.backgroundColor = .red
+        livesLabel.backgroundColor = .gray
+        currentAnswer.backgroundColor = .green
 
     }
 
